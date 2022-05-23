746d0bbc195603bde1130fd7178ae8641c96042b
James Garden <jamesegarden@outlook.com>
2022-05-23 16:33:55 +0100

    added first home page test

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomeIndexViewTests(TestCase):
+    def test_anonymous_user_gets_sign_in_up_links(self):
+        response = self.client.get(reverse('home:index'))
+        self.assertContains(response, "Log In")
+        self.assertContains(response, "Sign Up")
+
