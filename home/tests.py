27f7e7a059623285d45b42b6e2d5078c753d1ab8
James Garden <jamesegarden@outlook.com>
2022-06-02 14:09:37 +0100

    updated home page tests

--- home/tests.py
+++ home/tests.py
@@ -6,7 +6,16 @@ from django.urls import reverse
 
 class HomeIndexViewTests(TestCase):
     def test_anonymous_user_gets_sign_in_up_links(self):
-        response = self.client.get(reverse('home:index'))
+        response = self.client.get('/')
         self.assertContains(response, "Log In")
         self.assertContains(response, "Sign Up")
 
+    def test_signed_in_user_username(self):
+        username = "JimboBaggins"
+        email = "jbaggins@test.com"
+        password = "password"
+        User.objects.create_user(username=username, email=email, password=password)
+        response = self.client.post('/login/', {'username': username, 'password': password})
+        self.assertEquals(response.status_code, 200)
+        response = self.client.get('/')
+        self.assertContains(response, username)
