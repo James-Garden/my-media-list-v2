046108a71b54007e4f91ca37013383b10423548a
James Garden <jamesegarden@outlook.com>
2022-06-02 14:09:51 +0100

    added login tests

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class LoginTests(TestCase):
+    def test_login_view_loads(self):
+        response = self.client.get(reverse("user:login"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_invalid_login(self):
+        username = "bilbo"
+        password = "swaggins"
+        User.objects.create_user(username, 'bbaggins@theshire.com', password)
+        response = self.client.post(reverse("user:login"), {'username': 'user', 'password': 'password'})
+        self.assertNotContains(response, username)
+
+    def test_invalid_login_alert(self):
+        response = self.client.post(reverse("user:login"), {'username': 'user', 'password': 'password'})
+        self.assertContains(response, '<div class="alert')
+
+    def test_valid_login(self):
+        username = "bilbo"
+        password = "swaggins"
+        User.objects.create_user(username, 'bbaggins@theshire.com', password)
+        response = self.client.post(reverse("user:login"), {'username': username, 'password': password}, follow=True)
+        self.assertContains(response, username)
