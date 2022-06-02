b94f5b88ba83dd78ce6ac1cb69d4801ed540908c
James Garden <jamesegarden@outlook.com>
2022-06-02 14:10:48 +0100

    updated user urls

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,12 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
-app_name = 'profile'
+app_name = 'user'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('login', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('registration', views.register, name='registration'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    path('profile/', views.profile, name='profile'),
+    path('profile/<str:username>/', views.profile)
 ]
