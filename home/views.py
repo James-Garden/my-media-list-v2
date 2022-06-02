97b27645d10e06a319ca9a718e27893cb33b00ce
James Garden <jamesegarden@outlook.com>
2022-06-02 14:11:03 +0100

    updated home view

--- home/views.py
+++ home/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
-from django.conf import settings
-from django.contrib.auth import authenticate
+from utils.messages import notice
 
 
 def index(request):
-    user = authenticate(username='tes', password='password')
-    return render(request, 'home/index.html', context={
-        'user': user,
-    })
+    return render(request, 'home/index.html')
