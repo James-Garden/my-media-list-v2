3d5344c08e50d718603ed4fae0240b6b8a2163f8
James Garden <jamesegarden@outlook.com>
2022-06-02 14:11:33 +0100

    updated user/views.py for login, profile and registration

--- user/views.py
+++ user/views.py
@@ -1,5 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 
-def index(request):
-    return None
+class LoginForm(LoginView):
+    template_name = 'user/login.html'
+    next_page = 'user/profile.html'
+
+
+def register(request):
+    # When the user clicks 'submit'
+    if request.method == 'POST':
+        return None
+    # Load registration page
+    else:
+        return render(request, 'user/register.html')
+
+
+def profile(request, username=None):
+    if username is None:  # If the user has navigated to '/profile/'
+        if request.user.is_authenticated:  # If the user is looking at their own profile
+            profile_user = request.user
+
+        else:
+            return HttpResponseRedirect(reverse('user:login'))
+    else:  # If the user has navigated to '/profile/USERNAME'
+        profile_user = get_object_or_404(User, username=username)
+
+    return render(request, 'user/profile.html', context={
+        'profile': profile_user,
+    })
