fd3ddf1dfc2f6e2526f7f7933df6e688fd4c9b97
James Garden <jamesegarden@outlook.com>
2022-06-02 16:00:33 +0100

    added registration

--- user/views.py
+++ user/views.py
@@ -1,3 +1,6 @@
+from .forms import SignUpForm
+from utils.messages import notice
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView
 from django.http import HttpResponseRedirect
@@ -11,12 +14,26 @@ class LoginForm(LoginView):
 
 
 def register(request):
+    if request.user.is_authenticated:
+        notice(request, 'warning', 'Already logged in.')
+        return HttpResponseRedirect(reverse('user:profile'))
     # When the user clicks 'submit'
     if request.method == 'POST':
-        return None
+        form = SignUpForm(request.POST)
+        print(form.errors)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return HttpResponseRedirect(reverse('user:profile'))
+        else:
+            pass
     # Load registration page
     else:
-        return render(request, 'user/register.html')
+        form = SignUpForm()
+    return render(request, 'user/register.html', {'form': form})
 
 
 def profile(request, username=None):
