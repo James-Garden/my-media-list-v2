9b61e996962341a1687532946ac683cfa2eebc1d
James Garden <jamesegarden@outlook.com>
2022-06-06 13:50:02 +0100

    added media

--- myMediaList/settings.py
+++ myMediaList/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
     'user.apps.UserConfig',
+    'media.apps.MediaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
