8287aefbf48ab64fbbc73fb5f556bbef2be42be6
James Garden <jamesegarden@outlook.com>
2022-06-06 21:31:20 +0100

    updated tests to include birth_date, added tests for birth_dates

--- user/tests.py
+++ user/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
+from datetime import datetime, timedelta
 
 
 class LoginTests(TestCase):
@@ -62,15 +63,52 @@ class RegistrationTests(TestCase):
             'password1': "test_password",
             'password2': "test_password",
             'email': "!",
+            'birth_date': "01/01/1999",
         })
         self.assertContains(response, "Enter a valid email address.")
 
     def test_valid_register(self):
         response = self.client.post(reverse("user:registration"), {
-            'username': "test_username",
+            'username': "Test_username",
             'password1': "test_password",
             'password2': "test_password",
             'email': "email@example.com",
+            'birth_date': "01/01/1999",
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse("user:profile"))
+
+    def test_invalid_date_format(self):
+        response = self.client.post(reverse("user:registration"), {
+            'username': "Test_username",
+            'password1': "test_password",
+            'password2': "test_password",
+            'email': "email@example.com",
+            'birth_date': "01-01-1999",
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Enter a valid date.")
+
+    def test_dob_too_recent(self):
+        # Someone who was born today should not be allowed to register
+        response = self.client.post(reverse("user:registration"), {
+            'username': "Test_username",
+            'password1': "test_password",
+            'password2': "test_password",
+            'email': "email@example.com",
+            'birth_date': datetime.strftime(datetime.now(), "%d/%m/%Y"),
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "You must be at least 13 years old.")
+
+    def test_dob_thirteen_years(self):
+        # Someone who has just turned 13 should be allowed to register
+        response = self.client.post(reverse("user:registration"), {
+            'username': "Test_username",
+            'password1': "test_password",
+            'password2': "test_password",
+            'email': "email@example.com",
+            'birth_date': datetime.strftime(datetime.now() - timedelta(days=35152), "%d/%m/%Y"),
         })
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, reverse("user:profile"))
