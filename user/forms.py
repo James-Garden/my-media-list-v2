6700865a18ec7d88fdf772e025277896803e5520
James Garden <jamesegarden@outlook.com>
2022-06-08 16:26:29 +0100

    added edit profile form

--- user/forms.py
+++ user/forms.py
@@ -13,3 +13,19 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2', 'birth_date')
+
+
+class EditProfileForm(forms.ModelForm):
+    birth_date = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}))
+
+    def __init__(self, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            if visible.widget_type == 'select':
+                visible.field.widget.attrs['class'] = 'form-select'
+            else:
+                visible.field.widget.attrs['class'] = 'form-control'
+
+    class Meta:
+        model = Profile
+        fields = ['birth_date', 'birth_date_privacy', 'gender', 'avatar', 'location', 'bio', 'links']
