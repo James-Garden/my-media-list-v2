87cb39c4eedeeaf2cfc3832bb0e6f90ce1a28186
James Garden <jamesegarden@outlook.com>
2022-06-08 18:06:19 +0100

    moved context processor

--- myMediaList/settings.py
+++ myMediaList/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'myMediaList.context_processors.settings_nav_links'
+                'user.context_processors.settings_nav_links'
             ],
         },
     },
