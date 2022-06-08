6cf457e0b4129a46be0a3a749a3dde63a07ba3a8
James Garden <jamesegarden@outlook.com>
2022-06-08 16:26:16 +0100

    added username validator to signup form

--- user/forms.py
+++ user/forms.py
@@ -1,11 +1,14 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from utils.validators import validate_age, validate_username
+from .models import Profile
 
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=255)
-    birth_date = forms.DateField(help_text='Required. Format: DD/MM/YYYY', )
+    username = forms.CharField(max_length=24, validators=[validate_username])
+    birth_date = forms.DateField(help_text='Required. Format: DD/MM/YYYY', validators=[validate_age])
 
     class Meta:
         model = User
