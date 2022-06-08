d9af2d5b1878c7f4632f7c15f4a4b70b3ed13947
James Garden <jamesegarden@outlook.com>
2022-06-08 16:24:12 +0100

    created validators.py utility with username and age validators

---
+++ utils/validators.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from re import match
+
+from django.core.exceptions import ValidationError
+
+
+def validate_username(value):
+    if not match(r"^[A-Z][\w_]+$", str(value)):
+        raise ValidationError("Username must start with a capital letter and be alphanumeric.")
+
+
+def validate_age(value):
+    valid_birth_date = datetime.now()
+    valid_birth_date = valid_birth_date.replace(year=valid_birth_date.year - 13).date()
+    if value > valid_birth_date:
+        raise ValidationError("You must be at least 13 years old.")
