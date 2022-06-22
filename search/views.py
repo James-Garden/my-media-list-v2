418b8ccf8d4187fb2aed4b1ab6fbfd53ed5fe3cf
James Garden <jamesegarden@outlook.com>
2022-06-22 22:11:27 +0100

    added page navigation

--- search/views.py
+++ search/views.py
@@ -27,9 +27,27 @@ def index(request):
 
 
 def user_search(request, query: str) -> HttpResponse:
-    users = User.objects.filter(username__icontains=query)[:10]
+    results_per_page = 10
+    if 'page' in request.GET:
+        try:
+            offset = (int(request.GET['page']) - 1) * results_per_page
+            if offset < 0:
+                raise ValueError
+        except ValueError:
+            notice(request, "warning", "Invalid page number. Showing first page of results.")
+            offset = 0
+    else:
+        offset = 0
+    users = User.objects.filter(username__icontains=query)[offset*results_per_page:(offset+1) * results_per_page]
+    total_results = User.objects.filter(username__icontains=query).count()
+    pages = int(total_results // results_per_page) + (total_results % results_per_page > 0)
     return render(request, 'search/user_search.html', context={
         'query': query,
         'users': users,
         'current_user': request.user,
+        'page': offset+1,
+        'pages': range(1, pages+1),
+        'total_pages': pages,
+        'results_per_page': results_per_page,
+        'type': 'users',
     })
